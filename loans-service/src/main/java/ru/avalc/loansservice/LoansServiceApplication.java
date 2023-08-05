cc31742063736b389b6be7ec7c2783e21ee08148
Alexei Valchuk <a.valchukav@gmail.com>
2023-08-05 08:34:22 +0300

    create Loans service with initial settings

---
+++ loans-service/src/main/java/ru/avalc/loansservice/LoansServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.avalc.loansservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansServiceApplication.class, args);
+    }
+
+}
