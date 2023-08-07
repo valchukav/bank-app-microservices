0b6aae55b07ce4b4f836af17ce17e078bcb694f5
Alexei Valchuk <a.valchukav@gmail.com>
2023-08-07 16:29:22 +0300

    refactor: change Cards service to read properties from config server

--- cards-service/src/main/java/ru/avalc/cardsservice/controller/CardsController.java
+++ cards-service/src/main/java/ru/avalc/cardsservice/controller/CardsController.java
@@ -1,11 +1,17 @@
 package ru.avalc.cardsservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.avalc.cardsservice.config.CardsServiceConfig;
 import ru.avalc.cardsservice.model.Card;
 import ru.avalc.cardsservice.model.Customer;
+import ru.avalc.cardsservice.model.Properties;
 import ru.avalc.cardsservice.repo.CardsRepository;
 
 
@@ -19,15 +25,25 @@ import java.util.List;
 public class CardsController {
 
     private final CardsRepository repository;
+    private final CardsServiceConfig cardsServiceConfig;
 
     @Autowired
-    public CardsController(CardsRepository repository) {
+    public CardsController(CardsRepository repository, CardsServiceConfig cardsServiceConfig) {
         this.repository = repository;
+        this.cardsServiceConfig = cardsServiceConfig;
     }
 
     @PostMapping("/myCards")
     public List<Card> getCustomerLoans(@RequestBody Customer customer) {
 
         return repository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(),
+                cardsServiceConfig.getMailDetails());
+        return ow.writeValueAsString(properties);
     }
 }
---
+++ cards-service/src/main/java/ru/avalc/cardsservice/model/Properties.java
@@ -0,0 +1,22 @@
+package ru.avalc.cardsservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+/**
+ * @author Alexei Valchuk, 07.08.2023, email: a.valchukav@gmail.com
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Properties {
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+}
