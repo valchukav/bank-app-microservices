e46ae2dea56bccdcadb7c8f223da38fb4dddbd99
Alexei Valchuk <a.valchukav@gmail.com>
2023-08-07 18:22:03 +0300

    chore: mark services main methods with RefreshScope annotation

--- loans-service/src/main/java/ru/avalc/loansservice/LoansServiceApplication.java
+++ loans-service/src/main/java/ru/avalc/loansservice/LoansServiceApplication.java
@@ -2,7 +2,9 @@ package ru.avalc.loansservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class LoansServiceApplication {
 
