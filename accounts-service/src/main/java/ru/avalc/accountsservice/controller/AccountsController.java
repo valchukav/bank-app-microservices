05697facb32a04ca2ccc27932b32f5188e81401f
Alexei Valchuk <a.valchukav@gmail.com>
2023-08-09 16:47:25 +0300

    feat: implement rate limiter pattern to Account service

--- accounts-service/src/main/java/ru/avalc/accountsservice/controller/AccountsController.java
+++ accounts-service/src/main/java/ru/avalc/accountsservice/controller/AccountsController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import java.util.List;
  * @author Alexei Valchuk, 04.08.2023, email: a.valchukav@gmail.com
  */
 
+@RateLimiter(name = "accountsControllerRateLimiter")
 @RestController
 public class AccountsController {
 
