5fd32e49e45a74c83a55f3ddc58e764e60cd5c4b
Alexei Valchuk <a.valchukav@gmail.com>
2023-08-15 17:04:39 +0300

    feat: provide oauth2 authorization with client credentials grant type

---
+++ gateway-server/src/main/java/ru/avalc/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.avalc.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author Alexei Valchuk, 15.08.2023, email: a.valchukav@gmail.com
+ */
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer().jwt();
+
+        httpSecurity.csrf().disable();
+
+        return httpSecurity.build();
+    }
+}
